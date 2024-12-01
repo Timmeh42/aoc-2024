75a124f0b96be955d106d2c3eb42c5e574631f81
timmeh42 <tmeier42+github@gmail.com>
2024-12-01 08:12:20 +0200

    make it fast-er

--- days/1/solution.js
+++ days/1/solution.js
@@ -1,12 +1,18 @@
 module.exports = function (input) {
-    const column1 = input.trim().match(/^\d+/gm).map(Number).toSorted()
-    const column2 = input.trim().match(/\d+$/gm).map(Number).toSorted()
+    const column1 = input.match(/^\d+/gm).map(Number).toSorted()
+    const column2 = input.match(/\d+$/gm).map(Number).toSorted()
     let part1 = 0
     let part2 = 0
+    const freq1 = new Map()
+    const freq2 = new Map()
     for (let row = 0; row < column1.length; row++) {
         part1 += Math.abs(column1[row] - column2[row])
 
-        part2 += column1[row] * column2.filter(val => val === column1[row]).length
+        freq1.set(column1[row], (freq1.get(column1[row]) || 0) + 1)
+        freq2.set(column2[row], (freq2.get(column2[row]) || 0) + 1)
+    }
+    for (const [k, v] of freq1.entries()) {
+        part2 += k * v * (freq2.get(k) || 0)
     }
     return [part1, part2];
 }
